3b7eecb81ae4836d724813dab24b9dcbcd3ae5e4
Nikolay Radoev <nikolayradoev@gmail.com>
2023-07-18 13:45:46 -0400

    Structure refactor

--- ajax/xhr/xhr.js
+++ ajax/xhr/xhr.js
@@ -1,10 +1,10 @@
 
 function getRequest(id) {
   const getTextElement = document.getElementById("get-text");
-  const xhr = new window.XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.open("GET", `https://jsonplaceholder.typicode.com/todos/${id}`);
   xhr.addEventListener("readystatechange", function () {
-    if (xhr.readyState === window.XMLHttpRequest.DONE) {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 404) {
         getTextElement.textContent = `Erreur, l'item ${id} n'existe pas`;
         return;
@@ -20,16 +20,17 @@ function getRequest(id) {
 function postRequest() {
   const postTextElement = document.getElementById("post-text");
   const content = document.getElementById('server-message').value;
-  const xhr = new window.XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
   xhr.setRequestHeader("Content-Type", "application/json");
-  const data = { titre: "Message de XHR", contenu: `${content}` };
   xhr.addEventListener("readystatechange", function () {
-    if (xhr.readyState === window.XMLHttpRequest.DONE) {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
       const response = JSON.parse(xhr.responseText);
       postTextElement.textContent = xhr.responseText;
       console.log(response);
     }
   });
+
+  const data = { titre: "Message de XHR", contenu: `${content}` };
   xhr.send(JSON.stringify(data));
 }
